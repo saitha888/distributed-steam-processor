50fb8d2b7cb937f2d7e7f65a244ba7eb2e8b01f9
sonyj9 <sonyj9@fa24-cs425-1206.cs.illinois.edu>
2024-09-15 09:38:13 -0500

    testing and logging

--- logging.go
+++ logging.go
@@ -1,13 +1,14 @@
-
 package main
 
 import (
-	"fmt"
-	"net"
-	"os"
-	"os/exec"
-	"strings"
-	"io"
+    "fmt"
+    "net"
+    "os"
+    "os/exec"
+    "strings"
+    "io"
+    "strconv"
+    "time"
 )
 
 //global variable for port. machine number, log file name (different on each machine)
@@ -16,25 +17,26 @@ var machineNumber int = 6
 var filename string = "machine.6.log"
 
 func main() {
-	// check whether it's a server (receiver) or client (sender)
-	if len(os.Args) > 1 && os.Args[1] == "client" { // run client
-		grep := strings.Join(os.Args[2:], " ")
-		client(grep)
-	} else { // run server
-		server()
-	}
+    // check whether it's a server (receiver) or client (sender)
+    if len(os.Args) > 1 && os.Args[1] == "client" { // run client
+        grep := os.Args[2]
+        print("patten: ", grep)
+        client(grep)
+    } else { // run server
+        server()
+    }
 }
 
 func server() {
-	// listen for connection from other machine 
-	ln, err := net.Listen("tcp", ":" + port)
+    // listen for connection from other machine 
+    ln, err := net.Listen("tcp", ":" + port)
     if err != nil {
         fmt.Println(err)
         return
     }
 
-	// run subroutine to handle the connection
-	for {
+    // run subroutine to handle the connection
+    for {
         conn, err := ln.Accept()
         if err != nil {
             fmt.Println(err)
@@ -47,101 +49,142 @@ func server() {
 }
 
 func handleConnection(conn net.Conn) {
-	// Close the connection when we're done
+    // Close the connection when we're done
     defer conn.Close()
 
-	// Get the grep command
-	buf := make([]byte, 1024)
+    // Get the grep command
+    buf := make([]byte, 1024)
     n, _ := conn.Read(buf)
     message := string(buf[:n])
 
-	if message[:4] == "grep" { // recieving a grep command
-		command := message + " " + filename
-
-		// run the grep command on machine
-		cmd := exec.Command("sh", "-c", command)
-		output, err := cmd.CombinedOutput()
-		
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		
-		// send the result back to the initial machine
-		conn.Write(output)
+    if message[:4] == "grep" { // recieving a grep command
+        grep := message + " " + filename
+        // run the grep command on machine
+        cmd := exec.Command("sh", "-c", grep)
+        output, err := cmd.CombinedOutput()
+        
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+        
+        // send the result back to the initial machine
+        conn.Write(output)
+    } else if message[:5] == "client" {
+		totalLines := client("grep " + message[6:])
+		conn.Write([]byte(strconv.Itoa(totalLines)))
 	} else { // recieving a message to generate log file
-		// Open the file to write the contents
-		file, err := os.Create(filename)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer file.Close()
+        // Open the file to write the contents
+        file, err := os.Create(filename)
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+        defer file.Close()
 
-		// Write initial  chunk to the file
-		_, err = file.Write(buf[:n])
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+        // Write initial  chunk to the file
+        _, err = file.Write(buf[:n])
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
 
-		// Read from the connection in chunks and write to the file
-		for {
-			n, err := conn.Read(buf)
-			if err != nil {
-				if err == io.EOF {
-					break
-				}
-				fmt.Println(err)
-				return 
-			}
+        // Read from the connection in chunks and write to the file
+        for {
+            n, err := conn.Read(buf)
+            if err != nil {
+                if err == io.EOF {
+                    break
+                }
+                fmt.Println(err)
+                return 
+            }
 
-			// Write the chunk to the file
-			_, err = file.Write(buf[:n])
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-		}
-	}
-}
-
-func client(grep string) {
-	// have a list of addresses for other machines
-	ports := []string{"fa24-cs425-1201.cs.illinois.edu:8081", 
-						"fa24-cs425-1202.cs.illinois.edu:8082", 
-						"fa24-cs425-1203.cs.illinois.edu:8083", 
-						"fa24-cs425-1204.cs.illinois.edu:8084", 
-						"fa24-cs425-1205.cs.illinois.edu:8085", 
-						"fa24-cs425-1206.cs.illinois.edu:8086", 
-						"fa24-cs425-1207.cs.illinois.edu:8087", 
-						"fa24-cs425-1208.cs.illinois.edu:8088", 
-						"fa24-cs425-1209.cs.illinois.edu:8089",
-						"fa24-cs425-1210.cs.illinois.edu:8080"}
-	// loop through all other machines
-	for i := 0; i < len(ports); i++ {
-        // check if we're on initial machine
-		if i == machineNumber - 1 {
-			fmt.Println("here")
-			command := grep + " " + filename
-			cmd := exec.Command("sh", "-c", command)
-			output, err := cmd.CombinedOutput()
-			
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			fmt.Print(string(output))
-			return
-			
-		}
-		// connect to another machine and send grep command
-		sendCommand(ports[i], grep)
+            // Write the chunk to the file
+            _, err = file.Write(buf[:n])
+            if err != nil {
+                fmt.Println(err)
+                return
+            }
+        }
     }
 }
 
+func client(pattern string) int {
+    // Start the timer
+    start := time.Now()
+
+    // have a list of addresses for other machines
+    ports := []string{
+                        "fa24-cs425-1201.cs.illinois.edu:8081", 
+                        "fa24-cs425-1202.cs.illinois.edu:8082", 
+                        "fa24-cs425-1203.cs.illinois.edu:8083", 
+                        "fa24-cs425-1204.cs.illinois.edu:8084", 
+                        "fa24-cs425-1205.cs.illinois.edu:8085", 
+                        "fa24-cs425-1206.cs.illinois.edu:8086", 
+                        "fa24-cs425-1207.cs.illinois.edu:8087", 
+                        "fa24-cs425-1208.cs.illinois.edu:8088", 
+                        "fa24-cs425-1209.cs.illinois.edu:8089",
+                        "fa24-cs425-1210.cs.illinois.edu:8080",
+                    }
+    totalLines := 0
+    // loop through all other machines
+    for i := 0; i < len(ports); i++ {
+        // check if we're on initial machine
+        if i == machineNumber - 1 {
+            // fmt.Println("here")
+            command := "grep -nH " + pattern + " " + filename
+            cmd := exec.Command("sh", "-c", command)
+            output, err := cmd.CombinedOutput()
+            
+            command2 := "grep -c " + pattern + " " + filename
+            cmd2 := exec.Command("sh", "-c", command2)
+            output2, err2 := cmd2.CombinedOutput()
+
+            if err != nil {
+                fmt.Println(err)
+                return 0
+            }
+            if err2 != nil {
+                fmt.Println(err2)
+                return 0 
+            }
+            fmt.Print(string(output))
+            fmt.Print(string(output2))
+            //FIX LINE COUNT OF SELF MACHINE
+            // c, e := strconv.Atoi(output2)
+
+            // if e != nil {
+            //     fmt.Println(e)
+            //     return
+            // }
+        }
+        grep_response := "grep -nH " + pattern
+        grep_count := "grep -c " + pattern
+        //lineGrep := "grep -c" + pattern + " " + filename
+
+
+        // connect to another machine and send grep command
+        sendCommand(ports[i], grep_response)
+        //connect to another machine and send grep line command
+        lineCount := sendLineCommand(ports[i], grep_count)
+
+        totalLines += lineCount
+    }
+    //Print total lines
+    fmt.Print("Total line count: " + strconv.Itoa(totalLines) + "\n\n\n")
+
+    // Stop the timer
+    elapsed := time.Since(start)
+
+    // Output how long the command took
+    fmt.Printf("Grep command took %s to complete.\n", elapsed)
+    fmt.Println()
+    return totalLines
+}
+
 func sendCommand(port string, message string) {
-	// conect to the port
+    // conect to the port
     conn, err := net.Dial("tcp", port)
     if err != nil {
         fmt.Println(err)
@@ -149,21 +192,60 @@ func sendCommand(port string, message string) {
     }
     defer conn.Close()
 
-	// send the grep command to the machine
+    // send the grep command to the machine
     conn.Write([]byte(message))
 
-	// get the response from the machine
+    // get the response from the machine
     buf := make([]byte, 1024)
-	// loop through whole response and print it till it reaches the end
-	for {
+    // loop through whole response and print it till it reaches the end
+    for {
         n, err := conn.Read(buf)
         if err != nil {
             if err == io.EOF {
-				fmt.Println(err)
+
                 break 
             }
             return
         }
         fmt.Print(string(buf[:n])) 
     }
+}
+
+func sendLineCommand(port string, message string) int {
+    // conect to the port
+    conn, err := net.Dial("tcp", port)
+    if err != nil {
+        fmt.Println(err)
+        return 0
+    }
+    defer conn.Close()
+
+    // send the grep command to the machine
+    conn.Write([]byte(message))
+
+    // get the response from the machine
+    buf := make([]byte, 1024)
+    n, err := conn.Read(buf)
+    // loop through whole response and print it till it reaches the end
+    // for {
+    //     n, err := conn.Read(buf)
+    //     if err != nil {
+    //         if err == io.EOF {
+    //             fmt.Println(err)
+    //             break 
+    //         }
+    //         return
+    //     }
+    //     fmt.Print(string(buf[:n])) 
+    // }
+    fmt.Printf("Line count from machine: ")
+    fmt.Printf(string(buf[:n]) + "\n\n")
+    lineCountStr := string(buf[:n])
+    lineCountStr = strings.TrimSpace(lineCountStr)
+    lineCount, err := strconv.Atoi(lineCountStr)
+    if err != nil {
+        fmt.Printf("Error converting line count from %s to int: %v\n", port, err)
+        return 0
+    }
+    return lineCount
 }
--- test.go
+++ test.go
@@ -7,47 +7,72 @@ import (
     "math/rand"
     "time"
     "strings"
-    "testing"
 )
+var ports = []string{
+                        "fa24-cs425-1201.cs.illinois.edu:8081", 
+						"fa24-cs425-1202.cs.illinois.edu:8082", 
+						"fa24-cs425-1203.cs.illinois.edu:8083", 
+						"fa24-cs425-1204.cs.illinois.edu:8084", 
+						"fa24-cs425-1205.cs.illinois.edu:8085", 
+						"fa24-cs425-1206.cs.illinois.edu:8086", 
+						"fa24-cs425-1207.cs.illinois.edu:8087", 
+						"fa24-cs425-1208.cs.illinois.edu:8088", 
+						"fa24-cs425-1209.cs.illinois.edu:8089",
+						"fa24-cs425-1210.cs.illinois.edu:8080"
+                    }
 
-var ports = map[int]string{
-    // // 1: "fa24-cs425-1201.cs.illinois.edu:8081",
-    // 2: "fa24-cs425-1202.cs.illinois.edu:8082",
-    // // 3: "fa24-cs425-1203.cs.illinois.edu:8083",
-    // // 4: "fa24-cs425-1204.cs.illinois.edu:8084",
-    // // 5: "fa24-cs425-1205.cs.illinois.edu:8085",
-    6: "fa24-cs425-1206.cs.illinois.edu:8086",
-    // 7: "fa24-cs425-1207.cs.illinois.edu:8087",
-    // 8: "fa24-cs425-1208.cs.illinois.edu:8088",
-    // 9: "fa24-cs425-1209.cs.illinois.edu:8089",
-    // 10: "fa24-cs425-1210.cs.illinois.edu:8080",
+
+type Test struct {
+    testName string
+    countCheck int
+    patterns []string
 }
 
 func main() {
-    sendLogFiles()
-    output := sendCommand("fa24-cs425-1206.cs.illinois.edu:8086", "grep GET")
-    fmt.Printf(output)
-    // testAll()
+    fmt.Println("here")
+    // testSet := []Test{
+    //     {   
+    //         testName: "Infrequent Pattern Test",
+    //         countCheck: 500,
+    //         patterns: []string{"/mango"},
+    //     },
+    //     {
+    //         testName: "Frequent Pattern Test",
+    //         countCheck: 1000000,
+    //         patterns: []string{"/cherry"},
+    //     },
+    //     {
+    //         testName: "RegEx Pattern Test",      
+    //         countCheck: 1000000,
+    //         patterns: []string{"/kiwi", "/cherry"},
+    //     },
+    //     {
+    //         testName: "Occurs in One Machine Test",
+    //         countCheck: 100000,
+    //         patterns: []string{"/banana"},
+    //     },
+    //     {
+    //         testName: "Occurs in Some Machine Test",
+    //         countCheck: 50000,
+    //         patterns: []string{"/apple"},
+    //     },
+    // }
+    // testAll(testSet)
+    testRare()
 }
 
-func sendLogFiles() {
-
-    // Loop through all machines in the ports map
-    for machineNumber, address := range ports {
-        // Connect to the machine's server
-        conn, err := net.Dial("tcp", address)
-        if err != nil {
-            fmt.Printf("Error connecting to machine %d at %s: %v\n", machineNumber, address, err)
-            continue
-        }
-        defer conn.Close()
-
-
-        content:= generate_file_contents(100)
-        // Send a message indicating a log file is being transferred
-        conn.Write([]byte(content))
-        fmt.Printf("Log file '%s' sent to machine %d\n", logFileName, machineNumber)
+func sendLogFiles(content string, address string) {
+    // Connect to the machine's server
+    fmt.Println("in log files function")
+    conn, err := net.Dial("tcp", address)
+    if err != nil {
+        fmt.Println(err)
     }
+    defer conn.Close()
+    fmt.Println("made connection")
+
+    // write the file contents to the machine
+    conn.Write([]byte(content))
 }
 
 func sendCommand(port string, message string) string {
@@ -57,9 +82,10 @@ func sendCommand(port string, message string) string {
         fmt.Println(err)
         return ""
     }
+    fmt.Println("no error connecting")
     defer conn.Close()
 
-    // send the grep command to the machine
+    // send the to the machine
     conn.Write([]byte(message))
 
     var result strings.Builder
@@ -80,8 +106,170 @@ func sendCommand(port string, message string) string {
     return result.String()
 }
 
-func generate_file_contents(lines int) string {
 
+func testRare() string{
+    // file generation
+    patterns := []string{"/mango"}
+    pattern_count := 50
+    total_count := 1500
+
+    // grep command to send from machine
+    grep := "client /mango"
+
+    // generate files across all machines
+    for i := 0; i < len(ports); i++ {
+        fmt.Println("sending log files")
+        sendLogFiles(generate_file_contents(patterns, pattern_count, total_count), ports[i])
+    }
+    // send grep command from machine
+    totalLines := sendCommand("fa24-cs425-1207.cs.illinois.edu:8087", grep)
+    fmt.Println(totalLines)
+    return totalLines
+}
+
+func testFrequent(machine string) string{
+    // file generation
+    patterns := []string{"/cherry"}
+    pattern_count := 100000
+    total_count := 1500
+
+    // grep command to send from machine
+    grep := "/cherry"
+
+    // generate files across all machines
+    for i := 0; i < len(ports); i++ {
+        sendLogFiles(generate_file_contents(patterns, pattern_count, total_count), ports[i])
+    }
+    // send grep command from machine
+    return sendCommand(machine, grep)
+}
+
+func testRegex(machine string) string{
+    // file generation
+    patterns := []string{"/kiwi", "/cherry"}
+    pattern_count := 100000
+    total_count := 150000
+
+    // grep command to send from machine
+    grep := "/kiwi|/cherry"
+
+    // generate files across all machines
+    for i := 0; i < len(ports); i++ {
+        sendLogFiles(generate_file_contents(patterns, pattern_count, total_count), ports[i])
+    }
+    // send grep command from machine
+    return sendCommand(machine, grep)
+}
+
+func testOccursInOne(machine string) string{
+    // file generation for one machine
+    patterns := []string{"/banana"}
+    pattern_count := 100000
+    total_count := 150000
+
+    // grep command to send from machine
+    grep := "/banana"
+
+    // generate files across all machines
+    sendLogFiles(generate_file_contents(patterns, pattern_count, total_count), ports[0])
+    for i := 1; i < len(ports); i++ {
+        sendLogFiles(generate_file_contents(patterns, 0, total_count), ports[i])
+    }
+
+    // send grep command from machine
+    return sendCommand(machine, grep)
+}
+
+func testOccursInSome(machine string) string{
+    // file generation for one machine
+    patterns := []string{"/apple"}
+    pattern_count := 10000
+    total_count := 150000
+
+    // grep command to send from machine
+    grep := "/apple"
+
+    // generate files with pattern across half machines
+    for i := 0; i < len(ports); i++ {
+        if i % 2 == 0{
+            sendLogFiles(generate_file_contents(patterns, pattern_count, total_count), ports[i])
+        } else {
+            sendLogFiles(generate_file_contents(patterns, 0, total_count), ports[i])
+        }
+        
+    }
+
+    // send grep command from machine
+    return sendCommand(machine, grep)
+}
+
+
+// func testAll(testSet []Test) {
+
+//     for i := 0; i < 5; i++ {
+//         output := ""
+//         if i == 0 {
+//             output = testRare()
+//         } else if i == 1 {
+//             output = testFrequent(ports[m])
+//         } else if i == 2 {
+//             output = testRegex(ports[m])
+//         } else if i == 3 {
+//             output = testOccursInOne(ports[m])
+//         } else {
+//             output = testOccursInSome(ports[m])
+//         }
+        
+//         if !checkCount(output, testSet[i].countCheck) || !checkPattern(output, testSet[i].patterns[0], 50){
+//             fmt.Println("Test Failed: " + testSet[i].testName + " on machine " + ports[m])
+//         }
+//     }  
+// }
+
+
+// check functions for test cases
+func checkCount(output string, countCheck int) bool {
+    return strings.Count(output, "\n") == countCheck
+}
+
+// check that pattern is present in random lines
+func checkPattern(output string, pattern string, lines_to_check int) bool {
+
+    check_count := 0
+    // array of lines
+    lines_array := strings.Split(output, "\n")
+
+    // random number generator
+    rand.Seed(time.Now().UnixNano())
+
+    for check_count < lines_to_check {
+        // get random index
+        ind := rand.Intn(len(lines_array))
+
+        // check line
+        if !strings.Contains(lines_array[ind], pattern) {
+            return false
+        }
+        check_count += 1
+    }
+
+    return true
+}
+
+
+
+// helper functions for generating files 
+
+func generate_date() string {
+    start := time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC) // Start date: Jan 1, 2000
+    end := time.Now()
+    randomDuration := time.Duration(rand.Int63n(end.Unix() - start.Unix())) * time.Second
+    randomDate := start.Add(randomDuration)
+    return randomDate.Format("2006-01-02")
+}
+
+func generate_file_contents(patterns []string, pattern_count int, total_count int) string {
+    fmt.Println("generating file contents")
     // random number generator
     rand.Seed(time.Now().UnixNano())
 
@@ -93,61 +281,59 @@ func generate_file_contents(lines int) string {
 
     output := ""
 
+    ratio := total_count / pattern_count
+    curr_count := 0
+
     // create line_amount number of random lines
-    for i := 0; i <= lines; i++ {
+    for i := 0; i <= total_count; i++ {
         // random ip address
         ipAddress := fmt.Sprintf("%d.%d.%d.%d", rand.Intn(256), rand.Intn(256), rand.Intn(256), rand.Intn(256))
 
         // random date
-        start := time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC) // Start date: Jan 1, 2000
-        end := time.Now()
-        randomDuration := time.Duration(rand.Int63n(end.Unix() - start.Unix())) * time.Second
-        randomDate := start.Add(randomDuration)
-        date := randomDate.Format("2006-01-02")
+        date := generate_date()
 
         // endpoint type 
-        randomIndex1 := rand.Intn(len(endpointChoices))
-        endpoint := endpointChoices[randomIndex1]
+        randomIndex := rand.Intn(len(endpointChoices))
+        endpoint := endpointChoices[randomIndex]
 
         // random file name
-        randomIndex2 := rand.Intn(len(filenames))
-        filename := filenames[randomIndex2]
+        filename := ""
+        if i % ratio == 0 && curr_count != pattern_count{ // generate from wanted patterns
+            randomIndex = rand.Intn(len(patterns))
+            filename = patterns[randomIndex]
+            curr_count += 1
+        } else { // generate from default patterns
+            randomIndex = rand.Intn(len(filenames))
+            filename = filenames[randomIndex]
+        }
 
         contents := ipAddress +  " - - " + " [" + date + "] " + endpoint + " " + filename + " HTTP/1.0" + "\n"
         
         output += contents
     }
-    return output
-}
+    fmt.Println("for loop one done")
+    
+    for curr_count < pattern_count {
+        fmt.Println("for loop two")
+        // random ip address
+        ipAddress := fmt.Sprintf("%d.%d.%d.%d", rand.Intn(256), rand.Intn(256), rand.Intn(256), rand.Intn(256))
 
-type Test struct {
-    Name          string
-    GrepCommand   string
-    LogContents   string
-    ExpectedOutput string
-}
+        // random date
+        date := generate_date()
 
-testSet := []Test{
-    {
-        Name:          "Test 1: Search for error",
-        GrepCommand:   "grep 'ERROR'",
-        LogContents:   "INFO: Starting service\nERROR: Something went wrong\nINFO: Service started",
-        ExpectedOutput: "ERROR: Something went wrong",
-    },
-    {
-        Name:          "Test 2: Search for info",
-        GrepCommand:   "grep 'INFO'",
-        LogContents:   "INFO: Starting service\nERROR: Something went wrong\nINFO: Service started",
-        ExpectedOutput: "INFO: Starting service\nINFO: Service started",
-    },
-}
+        // endpoint type 
+        randomIndex1 := rand.Intn(len(endpointChoices))
+        endpoint := endpointChoices[randomIndex1]
 
+        // filename
+        randomIndex := rand.Intn(len(patterns))
+        filename := patterns[randomIndex]
 
-testAll() {
-    for test := range testSet {
-        output = sendCommand(test.GrepCommand)
-        if output != test.ExpectedOutput {
-            t.Errorf("Error with %s: Expected %s, but got %s", test.Name, expected, test.ExpectedOutput) // Fails the test with a formatted error message
-        }
+        contents := ipAddress +  " - - " + " [" + date + "] " + endpoint + " " + filename + " HTTP/1.0" + "\n"
+        
+        output += contents
+        curr_count += 1
     }
+    fmt.Println("generated done")
+    return output
 }
