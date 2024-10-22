6d9be716a4027aebeb011f1fbb9a86ceab3c91ba
sa60 <sa60@fa24-cs425-1201.cs.illinois.edu>
2024-10-22 13:25:31 -0500

    added grep case

--- logging.go
+++ logging.go
@@ -98,57 +98,66 @@ func commandLoop() {
         fmt.Print("> ") // CLI prompt
         scanner.Scan()
         command := scanner.Text()
-        if strings.HasPrefix(command, "client"){
-            go tcp.TcpClient(command[7:])
-        }
+        // if strings.HasPrefix(command, "client"){
+        //     go tcp.TcpClient(command[7:])
+        // }
+        args := strings.Fields(command)
 
-        switch command {
+        switch args[0] {
+            case "grep":   
+                if len(args) < 2 {
+                    fmt.Println("Error: Missing pattern parameter. Usage: grep <pattern>")
+                    continue
+                }
+                pattern := args[1]
+                fmt.Println(pattern)
+                tcp.TcpClient(pattern)
 
-        case "join":
-            udp.JoinSystem(addr)
-    
-            // Start pinging if joining the system
-            startPinging()
-        case "list_ring":
-            ring_map := udp.GetRing()
-            go udp.ListRing(ring_map)
-
-        case "list_mem":
-            membership_list := udp.GetMembershipList()
-            go udp.ListMem(membership_list)
+            case "join":
+                udp.JoinSystem(addr)
         
-        case "store":
-            go udp.PrintFiles("file-store")
-    
-        case "leave":
-            // Send a signal to stop the ping loop
-            if stopPing != nil {
-                close(stopPing) // Close the stopPing channel to stop the ping loop
+                // Start pinging if joining the system
+                startPinging()
+            case "list_ring":
+                ring_map := udp.GetRing()
+                go udp.ListRing(ring_map)
+
+            case "list_mem":
+                membership_list := udp.GetMembershipList()
+                go udp.ListMem(membership_list)
+            
+            case "store":
+                go udp.PrintFiles("file-store")
+        
+            case "leave":
+                // Send a signal to stop the ping loop
+                if stopPing != nil {
+                    close(stopPing) // Close the stopPing channel to stop the ping loop
+                }
+                go udp.LeaveList()
+            case "enable_sus":
+                // Toggle suspicion flag
+                suspicionEnabled = true
+                fmt.Println(suspicionEnabled)
+        
+            case "disable_sus":
+                // Disable suspicion mechanism
+                suspicionEnabled = false
+            
+            case "status_sus":
+                if suspicionEnabled {
+                    fmt.Println("Suspicion enabled")
+                } else {
+                    fmt.Println("Suspicion disabled")
+                }
+            
+            case "list_sus":
+                sus_list := udp.FindSusMachines()
+                go udp.ListMem(sus_list)
+
+
+            default:
+                fmt.Println("Unknown command. Available commands: list_mem, list_self, join, leave")
             }
-            go udp.LeaveList()
-        case "enable_sus":
-            // Toggle suspicion flag
-            suspicionEnabled = true
-            fmt.Println(suspicionEnabled)
-    
-        case "disable_sus":
-            // Disable suspicion mechanism
-            suspicionEnabled = false
-        
-        case "status_sus":
-            if suspicionEnabled {
-                fmt.Println("Suspicion enabled")
-            } else {
-                fmt.Println("Suspicion disabled")
-            }
-        
-        case "list_sus":
-            sus_list := udp.FindSusMachines()
-            go udp.ListMem(sus_list)
-
-
-        default:
-            fmt.Println("Unknown command. Available commands: list_mem, list_self, join, leave")
-        }
     }
 }
