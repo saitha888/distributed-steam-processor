2bac8b2733bbae4034a769f7471ed78abf07cc49
sonyj9 <sonyj9@fa24-cs425-1210.cs.illinois.edu>
2024-11-08 09:41:52 -0600

    membership

--- membership/helpers.go
+++ membership/helpers.go
@@ -2,6 +2,7 @@ package membership
 
 import (
     "fmt"
+	"net"
     "os"
     "strings"
     "time"
@@ -10,6 +11,7 @@ import (
     "log"
     "io/ioutil"
     "regexp"
+	"github.com/emirpasic/gods/maps/treemap"
 )
 
 // Function to randomly select an alive node in the system
@@ -242,4 +244,64 @@ func GetFileContents(filename string) string {
 
     result := combined.String()
     return result
+}
+
+func GetNodeID() string {
+    return node_id
+}
+
+func GetRing() *treemap.Map {
+    return ring_map
+}
+
+func GetMembershipList() []Node {
+	return membership_list
+}
+
+// Sends a message with contents to_send to target_node
+func SendMessage(target_node string, to_send string, node_to_send string) {
+    target_addr := target_node[:36]
+    conn, err := DialUDPClient(target_addr)
+    defer conn.Close()
+
+    message := to_send + " " + node_to_send
+    _, err = conn.Write([]byte(message))
+    if err != nil {
+        fmt.Println("Error sending fail message:", err)
+        return
+    }
+}
+
+// Sends a message that to_clear node is alive to node_id
+func SendAlive(node_id string, to_clear string, inc_num string) {
+
+    target_addr := node_id[:36]
+    conn, err := DialUDPClient(target_addr)
+    defer conn.Close()
+
+    message := "alive " + to_clear + " " + inc_num
+    _, err = conn.Write([]byte(message))
+    if err != nil {
+        fmt.Println("Error sending alive message:", err)
+        return
+    }
+}
+
+// Function to resolve and dial a UDP connection to a given address
+func DialUDPClient(target_addr string) (*net.UDPConn, error) {
+
+    // Resolve the UDP address
+    addr, err := net.ResolveUDPAddr("udp", target_addr)
+    if err != nil {
+        fmt.Println("Error resolving target address:", err)
+        return nil, err
+    }
+
+    // Dial UDP to the target node
+    conn, err := net.DialUDP("udp", nil, addr)
+    if err != nil {
+        fmt.Println("Error connecting to target node:", err)
+        return nil, err
+    }
+    return conn, nil
 }
--- membership/ring.go
+++ membership/ring.go
@@ -5,7 +5,83 @@ import (
     "crypto/sha256"
     "github.com/emirpasic/gods/maps/treemap"
     "encoding/binary"
+    "strings"
+    "strconv"
+    "sort"
 )
+
+func checkStatus(node string) string {
+    index := FindNode(node)
+    if index >= 0 {
+        return membership_list[index].Status
+    }
+    return "none"
+}
+
+// Turn the membership list global variable into a string
+func MembershiplistToString() string{
+    nodes := make([]string, 0)
+    for _,node := range membership_list {
+        current_node := node.NodeID + " " + node.Status + " " + strconv.Itoa(node.Inc)
+        nodes = append(nodes, current_node)
+    }
+    result := strings.Join(nodes, ", ")
+    return result
+}
+
+// Get the index of a machine in the list
+func FindNode(node_id string) int {
+    for index,node := range membership_list { 
+        if node_id == node.NodeID {
+            return index
+        }
+    }
+    return -1
+}
+
+// Change the status of a machine in the list
+func changeStatus(index int, message string){
+    membership_list[index].Status = message
+}
+
+// Change the status of a machine in the list
+func changeInc(index int, message int){
+    membership_list[index].Inc = message
+}
+
+func ListMem(list_to_print []Node) {
+    if len(list_to_print) == 0 {
+        fmt.Println("List is empty.")
+        return
+    }
+
+    nodeIDWidth := 56
+    ringIDWidth := 4
+    statusWidth := 4
+
+    sort.Slice(list_to_print, func(i, j int) bool {
+        return list_to_print[i].RingID < list_to_print[j].RingID
+    })
+
+    fmt.Printf("%-*s | %-*s | %-*s | %s | \n", ringIDWidth, "RingID", nodeIDWidth, "NodeID", statusWidth, "Status", "Incarnation #")
+    fmt.Println(strings.Repeat("-", nodeIDWidth+statusWidth+ringIDWidth+30))
+
+    // Go through membership list and print each entry
+    for _, node := range list_to_print {
+        fmt.Printf("%-*s | %s | %s  | %s\n",8,strconv.Itoa(node.RingID),node.NodeID, node.Status, strconv.Itoa(node.Inc))
+    }
+    fmt.Println()
+    fmt.Print("> ")
+}
+
+func FindNodeWithPort(port string) int {
+    for index,node := range(membership_list) {
+        if port == node.NodeID[:36] {
+            return index
+        }
+    }
+    return -1
+}
 
 // iteratorAt finds the iterator positioned at the given key
 func IteratorAt(ringMap *treemap.Map, start_val string) *treemap.Iterator {
